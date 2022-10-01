d89c44dffe0b638aa19eaec4ce2655390021dfb0
Nils Fo <nils.foerster@yahoo.de>
2022-10-01 13:32:51 +0200

    Updated ui for phases

---
+++ Assets/Scripts/UILogic.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using TMPro;
+using UnityEngine;
+
+public class UILogic : MonoBehaviour
+{
+    public TMP_Text gameStateText;
+    private GameState _gameState;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        _gameState = FindObjectOfType<GameState>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        gameStateText.text = "State: " + _gameState.currentState;
+    }
+}
