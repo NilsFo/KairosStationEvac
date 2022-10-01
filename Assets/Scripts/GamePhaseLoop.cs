aeb64c686a349ef6944da71e59c0e052f4b22bf2
Nils Fo <nils.foerster@yahoo.de>
2022-10-01 13:21:45 +0200

    Implemented logic & loop between phases

---
+++ Assets/Scripts/GamePhaseLoop.cs
@@ -0,0 +1,48 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GamePhaseLoop : MonoBehaviour
+{
+    public GameState myGameState;
+    public int phaseLength = 10;
+    private float timer;
+    private GameState.State currentState = GameState.State.Unknown;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        myGameState.currentState = GameState.State.PlanningPhase;
+        timer = 0;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        timer += Time.deltaTime;
+        currentState = myGameState.currentState;
+
+        if (currentState == GameState.State.WinState || currentState == GameState.State.Unknown)
+        {
+            return;
+        }
+
+        if (timer >= phaseLength)
+        {
+            timer = 0;
+            NextPhase();
+        }
+    }
+
+    public void NextPhase()
+    {
+        if (currentState == GameState.State.PlanningPhase)
+        {
+            myGameState.currentState = GameState.State.PlayPhase;
+        }
+        else
+        {
+            myGameState.currentState = GameState.State.PlanningPhase;
+        }
+    }
+}
--- Assets/Scripts/GameState.cs
+++ Assets/Scripts/GameState.cs
@@ -1,18 +1,82 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.Events;
 
 public class GameState : MonoBehaviour
 {
+    public enum State
+    {
+        Unknown,
+        PlayPhase,
+        PlanningPhase,
+        WinState
+    }
+
+    private bool firstCleanDone = false;
+    public State currentState = State.Unknown;
+    private State _lastKnownState;
+    public UnityEvent onStatePlaying;
+    public UnityEvent onStatePlanning;
+    public UnityEvent onStateWin;
+    public UnityEvent onResetGameplay;
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        if (onStatePlaying == null) onStatePlaying = new UnityEvent();
+        if (onStatePlanning == null) onStatePlanning = new UnityEvent();
+        if (onStateWin == null) onStateWin = new UnityEvent();
+        if (onResetGameplay == null) onResetGameplay = new UnityEvent();
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        if (!firstCleanDone)
+        {
+            firstCleanDone = true;
+            ResetAndCleanUp();
+        }
+
+        if (_lastKnownState != currentState)
+        {
+            OnStateChanged();
+            _lastKnownState = currentState;
+        }
+    }
+
+    public void WinGame()
+    {
+        print("A winner is you!");
+        currentState = State.WinState;
+    }
+
+    private void OnStateChanged()
+    {
+        print("The state has changed. From: '" + _lastKnownState + "' to '" + currentState);
+        //TODO callback to other elements??
+
+        switch (currentState)
+        {
+            case State.PlanningPhase:
+                ResetAndCleanUp();
+                onResetGameplay.Invoke();
+                break;
+            case State.PlayPhase:
+                onStatePlaying.Invoke();
+                break;
+            case State.WinState:
+                onStateWin.Invoke();
+                break;
+        }
+    }
+
+    private void ResetAndCleanUp()
+    {
+        onResetGameplay.Invoke();
+        // TODO Implement
+        // When this is called, reset all objects for the next loop
     }
 }
