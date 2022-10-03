c0b668aad97b5e4216da8796d5bea876b3dc142a
Nils Fo <nils.foerster@yahoo.de>
2022-10-03 20:43:59 +0200

    Big explosion now visible when winning

--- Assets/Scripts/GamePhaseLoop.cs
+++ Assets/Scripts/GamePhaseLoop.cs
@@ -156,14 +156,15 @@ public class GamePhaseLoop : MonoBehaviour
             if (timer >= phaseLengthEvac)
             {
                 timer = 0;
-                if (myGameState.levelWon)
-                {
-                    SetPhasePlanning();
-                }
-                else
-                {
-                    SetPhaseExplosion();
-                }
+                // if (myGameState.levelWon)
+                // {
+                //     SetPhasePlanning();
+                // }
+                // else
+                // {
+                //     SetPhaseExplosion();
+                // }
+                SetPhaseExplosion();
             }
         }
     }
